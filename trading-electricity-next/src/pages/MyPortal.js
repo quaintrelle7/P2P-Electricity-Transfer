247142f33c99b76aa58f04499882cc8f6081ffb6
Sharayu <sharayushendre7@gmail.com>
2023-05-14 19:57:20 +0530

    changed UI

--- trading-electricity-next/src/pages/MyPortal.js
+++ trading-electricity-next/src/pages/MyPortal.js
@@ -1,131 +1,206 @@
-import React from 'react'
-import { AuctionContract } from 'blockend/interact'
-import { useState, useEffect } from 'react';
-import web3 from 'blockend/web3';
+import React from "react";
+import { AuctionContract } from "blockend/interact";
+import { useState, useEffect } from "react";
+import web3 from "blockend/web3";
+
+import Link from "next/link";
 
 export default function MyPortal() {
+  const [account, setAccount] = useState("");
+  const [state, setState] = useState({
+    units: "",
+    price: "",
+    lockingPeriod: "",
+    errorMessage: "",
+    loading: false,
+  });
 
-    const[account, setAccount] = useState('');
-    const[state, setState] = useState({
-        units:'',
-        price:'',
-        lockingPeriod:'',
-        errorMessage:'',
-        loading: false
-    });
-   
-    function handleInputChange(event){
-        const{name, value} = event.target;
-        setState(prevState => ({
-            ...prevState, [name]: value
-        }));
-    }
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
 
-    useEffect(() => {
-            const getAccount = async () => {
-                const accounts = await web3.eth.getAccounts();
-                setAccount(accounts[0]);
-            };
-            getAccount();
-            }, []);
-    
-    const handleSubmit = async(event) =>{
-        
-        event.preventDefault();
-
-        
-
-        setState({loading:true});
-        try{
-
-            //get the account address to make the transaction
-            const accounts = await web3.eth.requestAccounts();
-
-            const tx = await AuctionContract.methods.listElectricity
-            (state.units, state.price, state.lockingPeriod)
-            .send({ from: accounts[0] });
-
-            console.log(tx);
-
-            
-
-        }
-        catch(err){
-            setState({errorMessage: err.message});
-
-        }
-        
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
     };
+    getAccount();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setState({ loading: true });
+    try {
+      //get the account address to make the transaction
+      const accounts = await web3.eth.requestAccounts();
+
+      const tx = await AuctionContract.methods
+        .listElectricity(state.units, state.price, state.lockingPeriod)
+        .send({ from: accounts[0] });
+
+      console.log(tx);
+    } catch (err) {
+      setState({ errorMessage: err.message });
+    }
+  };
 
   return (
     <div>
-    <h1 style={{color:'white', fontFamily:"Arial", marginBottom:"40px"}}>My Energy Portal!</h1>
+      <h1 style={{ color: "white", fontFamily: "Arial", marginBottom: "40px" }}>
+        My Energy Portal
+      </h1>
 
-    <div className='myportal' style={{display:"flex", flexDirection:"row", flexWrap: "nowrap", justifyContent:"space-around"}}>
+      <Link
+        href="/"
+        style={{
+          color: "#00ffef",
+          fontSize: "20px",
+          fontWeight: "Bold",
+          textDecoration: "underline",
+        }}
+      >
+        Go back
+      </Link>
 
+      <div
+        className="myportal"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "nowrap",
+          justifyContent: "space-around",
+          marginTop: "20px",
+        }}
+      >
         <div>
-            <div className="myportal-left" style={{border: "1px solid white", padding: "40px", borderRadius:"10px", width: "55vh", height:"20vw", backgroundColor:"#bdf6ea"}}>
-
-            <h3 style={{fontFamily:"Arial", marginBottom:"20px", marginLeft:"60px"}}>Input Energy Details</h3>
+          <div
+            className="myportal-left"
+            style={{
+              border: "1px solid white",
+              padding: "40px",
+              borderRadius: "10px",
+              width: "55vh",
+              height: "20vw",
+              backgroundColor: "#bdf6ea",
+            }}
+          >
+            <h3
+              style={{
+                fontFamily: "Arial",
+                marginBottom: "20px",
+                marginLeft: "60px",
+              }}
+            >
+              Input Energy Details
+            </h3>
 
             <form onSubmit={handleSubmit}>
-                <div>
-                    <input name="units"
-                           value={state.units}
-                           onChange={handleInputChange}
-                           placeholder='    Units in KW' 
-                           style={{marginRight:"0px", width:"42vh", borderColor:"#0bcda4"}}
-                           required="true"></input>
-                </div>
+              <div>
+                <input
+                  name="units"
+                  value={state.units}
+                  onChange={handleInputChange}
+                  placeholder="    Units in KW"
+                  style={{
+                    marginRight: "0px",
+                    width: "42vh",
+                    borderColor: "#0bcda4",
+                  }}
+                  required="true"
+                ></input>
+              </div>
 
-                <div>
-                    <input name="price"
-                           value={state.price}
-                           onChange={handleInputChange}
-                           placeholder='    Price in Eth' 
-                           style={{marginTop:"20px", width:"42vh", borderColor:"#0bcda4"}}
-                           required="true"></input>
-                </div>
-               
-               
+              <div>
+                <input
+                  name="price"
+                  value={state.price}
+                  onChange={handleInputChange}
+                  placeholder="    Price in Eth"
+                  style={{
+                    marginTop: "20px",
+                    width: "42vh",
+                    borderColor: "#0bcda4",
+                  }}
+                  required="true"
+                ></input>
+              </div>
 
-                    <input name="lockingPeriod"
-                           value={state.lockingPeriod}
-                           onChange={handleInputChange}
-                           placeholder='    Lock-In Period' 
-                           style={{marginTop:"10px",width:"42vh", borderColor:"#0bcda4"}}
-                           required="true"></input>
-                
-                
-                <button className='btn' style={{marginTop:"30px", marginLeft:"10vh", backgroundColor:"#0bcda4", color:"black", borderColor:"white"}}>Submit</button>
+              <input
+                name="lockingPeriod"
+                value={state.lockingPeriod}
+                onChange={handleInputChange}
+                placeholder="    Lock-In Period"
+                style={{
+                  marginTop: "10px",
+                  width: "42vh",
+                  borderColor: "#0bcda4",
+                }}
+                required="true"
+              ></input>
+
+              <button
+                className="btn"
+                style={{
+                  marginTop: "30px",
+                  marginLeft: "10vh",
+                  backgroundColor: "#0bcda4",
+                  color: "black",
+                  borderColor: "white",
+                }}
+              >
+                Submit
+              </button>
             </form>
-            </div>
+          </div>
         </div>
 
-
+        <div>
+          <div
+            className="myportal-right"
+            style={{
+              border: "1px solid white",
+              padding: "40px",
+              borderRadius: "10px",
+              width: "55vh",
+              backgroundColor: "#bdf6ea",
+              height: "20vw",
+            }}
+          >
+            <h3
+              style={{
+                fontFamily: "Arial",
+                marginBottom: "20px",
+                marginLeft: "60px",
+              }}
+            >
+              My Recent Purchase
+            </h3>
             <div>
-            <div className="myportal-right" style={{border: "1px solid white", padding: "40px", borderRadius:"10px", width: "55vh", backgroundColor:"#bdf6ea", height:"20vw"}}>
+              <h4 style={{ fontFamily: "Arial", marginTop: "40px" }}>
+                Listing Address: {account}{" "}
+              </h4>
+              <h4 style={{ fontFamily: "Arial", marginTop: "20px" }}>
+                Seller Address:{" "}
+              </h4>
 
-            <h3 style={{fontFamily:"Arial", marginBottom:"20px", marginLeft:"60px"}}>My Recent Purchase</h3>
-                <div>
-                    <h4 style={{fontFamily:"Arial", marginTop:"40px"}}>Listing Address: {account} </h4>
-                    <h4 style={{fontFamily:"Arial", marginTop:"20px"}}>Seller Address: </h4>
-
-
-                   <h4 style={{fontFamily:"Arial", marginTop:"20px"}}>Energy Units:{state.units} </h4>
-                   <h4 style={{fontFamily:"Arial", marginTop:"20px"}}>Total Price: </h4>
-                   <h4 style={{fontFamily:"Arial", marginTop:"20px"}}>Source of Energy: </h4>
-
-
-
-
-                </div>
-
-               
-                
+              <h4 style={{ fontFamily: "Arial", marginTop: "20px" }}>
+                Energy Units:{" "}
+              </h4>
+              <h4 style={{ fontFamily: "Arial", marginTop: "20px" }}>
+                Total Price:{" "}
+              </h4>
+              <h4 style={{ fontFamily: "Arial", marginTop: "20px" }}>
+                Source of Energy:{" "}
+              </h4>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
--- trading-electricity-next/src/pages/index.js
+++ trading-electricity-next/src/pages/index.js
@@ -1,36 +1,36 @@
-import web3 from 'blockend/web3'
-import { useEffect, useState } from 'react';
-import  {AuctionContract}  from 'blockend/interact';
-import Link from 'next/link';
+import web3 from "blockend/web3";
+import { useEffect, useState } from "react";
+import { AuctionContract } from "blockend/interact";
+import Link from "next/link";
+import MyFooter from "./MyFooter";
+import Prosumer from "./Prosumer";
+import { AiFillGithub, AiFillTwitterCircle } from "react-icons/ai";
 
 export default function Home() {
-
   web3.eth.getAccounts().then(console.log);
 
-   const [account, setAccount] = useState('');
+  const [account, setAccount] = useState("");
 
   useEffect(() => {
-  const getAccount = async () => {
-    const accounts = await web3.eth.getAccounts();
-    setAccount(accounts[0]);
-  };
-  getAccount();
-}, []);
+    const getAccount = async () => {
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    getAccount();
+  }, []);
 
-const[data, setData] = useState('');
+  const [data, setData] = useState("");
 
-useEffect(()=>{
-  async function fetchData(){
-    const accounts = await web3.eth.requestAccounts();
-    const result = await AuctionContract.methods.getAllListings().call();
-    setData(result);
-  }
-  fetchData();
+  useEffect(() => {
+    async function fetchData() {
+      const accounts = await web3.eth.requestAccounts();
+      const result = await AuctionContract.methods.getAllListings().call();
+      setData(result);
+    }
+    fetchData();
+  }, []);
 
-}, [])
-
-
-const[listingAddress, setListingAddress] = useState('');
+  const [listingAddress, setListingAddress] = useState("");
 
   // useEffect(() => {
   //   const getListingAddress = async () => {
@@ -43,65 +43,116 @@ const[listingAddress, setListingAddress] = useState('');
   //   getListingAddress();
   // }, [AuctionContract]);
 
-   const handleSubmit = async( _listingId,  _units) =>{
-      
-        
+  const handleSubmit = async (_listingId, _units) => {
+    //get the account address to make the transaction
+    const accounts = await web3.eth.requestAccounts();
 
-      
-
-            //get the account address to make the transaction
-            const accounts = await web3.eth.requestAccounts();
-
-            const tx = await AuctionContract.methods.placeBid
-            (_listingId, _units)
-            .send({ from: accounts[0] });
-
-
-            
-
-      
-        
-    };
+    const tx = await AuctionContract.methods
+      .placeBid(_listingId, _units)
+      .send({ from: accounts[0] });
+  };
 
   return (
-
-    
     <div>
- <div> 
-            <h1 style={{color:'white', fontFamily:"Arial"}}>Welcome to P2P Electricity Transfer Network!</h1>
-            {/* <button className='btn'>Connect Wallet</button> */}
-           
-    </div>
-    
-    <div style={{display:"flex", justifyContent:"space-between"}}>
-    <h3  style={{color:'white', marginTop:'70px'}}>Hello  {account}! </h3>
-    <Link href="/MyPortal" style={{color:'white', marginTop:'70px', fontSize:"20px", fontWeight:"Bold"}}>Go to MyPortal </Link>
-    </div>
-    <h2 style={{color:'white', marginTop:'70px', fontFamily:"Arial"}}>Ongoing Bids</h2>
+      <div>
+        <h1 style={{ color: "white", fontFamily: "Arial" }}>
+          Welcome to P2P Electricity Transfer Network
+        </h1>
+        {/* <button className='btn'>Connect Wallet</button> */}
+      </div>
 
-    <table className='table-1'>
-    <tbody>
-      <tr>
-        <th>Seller Address</th>
-        <th>Units Available</th>
-        <th>Price</th>
-        <th>Buy</th>
-      </tr>
-    
-      {data && data.map((item, index)=>(
-         <tr key={index}>
-        <td>{item.prosumer}</td>
-        <td>{item.units}</td>
-        <td>{item.price}</td>
-        <td><button onSubmit={handleSubmit(item.listingId, item.units)} style={{backgroundColor:"#9cecdb", width:"12vh", height:"30px", borderRadius:"5px", cursor:"pointer"}}>buy</button></td>
-      </tr>
-      ))}
-    
-     
-      </tbody>
-    </table>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h3 style={{ color: "white", marginTop: "70px" }}>Hello {account}! </h3>
+        <Link
+          href="/MyPortal"
+          style={{
+            color: "#00ffef",
+            marginTop: "70px",
+            fontSize: "20px",
+            fontWeight: "Bold",
+            textDecoration: "underline",
+          }}
+        >
+          Go to MyPortal{" "}
+        </Link>
+      </div>
+      <h2 className="portal-style">Available Electricity Units for Sale</h2>
 
+      <table className="table-1">
+        <tbody>
+          <tr>
+            <th>Seller Address</th>
+            <th>Units Available</th>
+            <th>Price</th>
+            <th>Buy</th>
+          </tr>
+
+          {data &&
+            data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.prosumer}</td>
+                <td>{item.units}</td>
+                <td>{item.price}</td>
+                <td>
+                  <button
+                    onSubmit={handleSubmit(item.listingId, item.units)}
+                    style={{
+                      backgroundColor: "#9cecdb",
+                      width: "12vh",
+                      height: "30px",
+                      borderRadius: "5px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    buy
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      {/* <MyFooter/> */}
+
+      <hr style={{ marginTop: "50px", borderTop: "1px solid" }}></hr>
+
+      <div style={{ marginTop: "20px" }}>
+        <Link
+          target="_blank"
+          href="https://github.com/quaintrelle7/P2P-Electricity-Transfer"
+        >
+          <AiFillGithub color="white" fontSize={"25px"} />
+        </Link>
+
+        <Link href="https://twitter.com/ARPITKU80579385" target="_blank">
+          <AiFillTwitterCircle
+            color="white"
+            fontSize={"25px"}
+            style={{ marginLeft: "20px" }}
+          />
+          <text
+            style={{
+              color: "white",
+              marginLeft: "5px",
+              marginTop: "3px",
+              position: "absolute",
+            }}
+          >
+            @dungexn
+          </text>
+        </Link>
+        <Link target="_blank" href="https://twitter.com/qua_intrelle7">
+          <text
+            style={{
+              color: "white",
+              marginLeft: "90px",
+              marginTop: "3px",
+              position: "absolute",
+            }}
+          >
+            @qua_intrelle7
+          </text>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
-
